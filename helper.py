7cedef89b4947b44aad8d644600334273a35bc2e
Masum Billal <2643770+proafxin@users.noreply.github.com>
2021-01-14 22:53:11 +0600

    created dataframe from url directly

--- helper.py
+++ helper.py
@@ -1,4 +1,6 @@
 from json import loads
+from requests import get
+from pandas import DataFrame
 
 with open('key.json') as _f:
     API_KEY = loads(_f.read())['api_key']
@@ -9,3 +11,11 @@ def form_url(url):
     url = url.format(API_KEY)
 
     return url
+
+def get_data(url):
+    url = form_url(url)
+    response = get(url)
+    content = response.content.decode()
+    data = loads(content)
+    
+    return DataFrame(data)
